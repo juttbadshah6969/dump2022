2a350555986c33d0b1843a61b33dcb492d4ba0ed
JuttBadshah <85131357+juttbadshah6969@users.noreply.github.com>
2021-11-10 23:57:40 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,10 +6,8 @@ echo
 echo "Update Command, Patient Bro ..!"
 echo
 cd
-cd
-cd
 cd /data/data/com.termux/files/home
 sleep 1
-rm -r dump2020
+rm -rf dump2020
 sleep 5
 git clone https://github.com/juttbadshah6969/dump2022
