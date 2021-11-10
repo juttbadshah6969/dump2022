0f3f8f7fb4f0bb98941097641392c76ebd252203
JuttBadshah <85131357+juttbadshah6969@users.noreply.github.com>
2021-11-10 22:42:17 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ echo "Install All You Need, Patient Bro ..!"
 echo
 apt update && apt upgrade
 
+apt update && apt upgrade
 apt install python2
 pip2 install mechanize
 pip2 install requests bs4
+pip2 install mechanize lolcat
 apt install git
 pip2 install futures
 termux-setup-storage
