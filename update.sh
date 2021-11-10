dba954cf8ca8986e93eafd24391deafd2c750080
JuttBadshah <85131357+juttbadshah6969@users.noreply.github.com>
2021-11-10 22:28:24 +0500

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,16 @@
+#Facebook : Jutt Badshah
+#Github   : https://github.com/juttbadshah6969
+#Whatsapp : +923100209977
+clear
+echo
+echo "Update Command, Patient Bro ..!"
+echo
+cd
+cd
+cd
+cd storage
+cd downloads
+rm -rf dump2020
+git clone https://github.com/juttbadshah6969/dump2022
+cd dump2022
+python2 ext.so
